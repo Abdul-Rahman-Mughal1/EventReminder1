adf71364f8f06c3ec79c37a7fc9ea6e8b71fd15e
Abdul-Rahman-Mughal1 <abdulrahmanmughal099@gmail.com>
2024-12-25 23:16:11 +0500

    Add project files.

---
+++ EventReminder/Controllers/EventController.cs
@@ -0,0 +1,81 @@
+﻿using EventReminder.Data;
+using EventReminder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventReminder.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var events = await _context.Events.ToListAsync();
+            return View(events);
+        }
+
+        // GET: Event/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+        }
+
+        // GET: Event/Countdown/{id}
+        public async Task<IActionResult> Countdown(int id)
+        {
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Event/Delete/{id}
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Event/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Events.FindAsync(id);
+            _context.Events.Remove(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ EventReminder/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EventReminder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventReminder.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; } // Define your DbSet
+    }
+}
